7b7f41fc08efeea4b8dc9b18ec11dcdadb36b17e
jrcoste6 <jct702@gmail.com>
2025-06-04 15:27:34 -0400

    Continuación

--- funcionesRecursivas.js
+++ funcionesRecursivas.js
@@ -2,12 +2,8 @@ function palabras (input) {
 {
     if (input == "negro") {
     return "Palabra secreta adivinada";
-    } else console.log("Le quedan 5 intentos");
-
-    
-    
+    } else console.log("Le quedan 5 intentos"); 
 } 
     
 }
-
-console.log(palabras("blanco"));
+console.log(palabras("negro"));
