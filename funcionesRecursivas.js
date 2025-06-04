6fe8602e5eeb7226f68be19619c5abbded4ac50d
jrcostedev <jct702@gmail.com>
2025-06-03 23:21:31 -0400

    Ejercicio propio de Funciones Recursivas

---
+++ funcionesRecursivas.js
@@ -0,0 +1,11 @@
+function palabras (input) {
+{
+    if (input == "negro") {
+    return "Palabra secreta adivinada";
+    } else console.log("Le quedan 5 intentos");
+    
+} 
+    
+}
+
+console.log(palabras("negro"));
